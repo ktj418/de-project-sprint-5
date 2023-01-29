266bf559436195b85b7b6831ca820c12298cb1df
ktaj.ja <ktaj.ja@gmail.com>
2023-01-29 19:42:18 +0300

    3rd submit

--- src/dags/examples/project/api_to_stg_dag.py
+++ src/dags/examples/project/api_to_stg_dag.py
@@ -1,12 +1,15 @@
 import requests
 import pandas as pd
 from typing import List
+import numpy as np
 
 from airflow import DAG
 from airflow.providers.postgres.operators.postgres import PostgresOperator
 from airflow.providers.postgres.hooks.postgres import PostgresHook
 from airflow.operators.python_operator import PythonOperator
 import pendulum
+#import time
+#from datetime import datetime, timedelta
 
 
 postgres_conn_id = 'PG_WAREHOUSE_CONNECTION'
@@ -15,6 +18,8 @@ headers = {
     'X-Nickname': 'kt4ja418', 
     'X-Cohort': '7', 
     'X-API-KEY': '25c27781-8fde-4b30-a22e-524044a7580f'}
+
+business_dt = '{{ yesterday_ds }}' # за предыдущий день
 
 def get_couriers(url, pg_table, pg_schema, offset=0, headers = headers):
     couriers_df = pd.DataFrame(columns = ['id', 'courier_data'])
@@ -37,46 +42,66 @@ def get_couriers(url, pg_table, pg_schema, offset=0, headers = headers):
     row_count = couriers_df.to_sql(pg_table, engine, schema=pg_schema, if_exists='append', index=False)
     print(f'{row_count} rows was inserted')
 
-def get_deliveries(url, pg_table, pg_schema, offset=0, headers = headers):
+def curr_delivery_id(ti):
+    postgres_hook = PostgresHook(postgres_conn_id)
+    engine = postgres_hook.get_sqlalchemy_engine()
+    value = engine.execute("SELECT max(id) from stg.api_deliveries").fetchall()[0][0]
+
+    ti.xcom_push(key='id_num', value=value)
+
+def get_deliveries(date, pg_table, pg_schema, ti, headers = headers):
+    offset = 0
+    headers = {'X-Nickname': 'kt4ja418', 'X-Cohort': '7', 'X-API-KEY': '25c27781-8fde-4b30-a22e-524044a7580f'}
+    x = [1]
+    
+    id_num = ti.xcom_pull(key='id_num')
+    if id_num is None:
+        id_num = 0
+    else:
+        id_num += 1
+    print('id_num = ', id_num)
+
     deliveries_df = pd.DataFrame(columns = ['id', 'delivery_data'])
-    productivity_counter = 1
-    while productivity_counter > 0:
-        full_url = url + str(offset)
-        df = pd.DataFrame({'delivery_data':requests.get(full_url, headers=headers).json()}).reset_index(level=0)
+
+    while len(x) > 0:
+        url = f'https://d5d04q7d963eapoepsqr.apigw.yandexcloud.net/deliveries?from={date}%2000:00:00&to={date}%2023:59:59&sort_field=date&sort_direction=asc&limit=50&offset={offset}'
+        print(url)
+        print(offset)
+        x = requests.get(url, headers=headers).json()
+        df = pd.DataFrame({'delivery_data':requests.get(url, headers=headers).json()}).reset_index(level=0)
         df.columns = ['id', 'delivery_data']
-        offset+=50
         deliveries_df = pd.concat([deliveries_df, df])
-        productivity_counter = len(df)
+        offset+=50
+    
     deliveries_df = deliveries_df.reset_index()
     deliveries_df['id'] = deliveries_df.index
     deliveries_df = deliveries_df[['id', 'delivery_data']].copy()
     deliveries_df['delivery_data'] = deliveries_df['delivery_data'].astype(str).replace('\'', '"')
+    deliveries_df['id'] = deliveries_df['id'] + id_num
+    print(deliveries_df.head())
 
     # загрузка в стейджинг
     postgres_hook = PostgresHook(postgres_conn_id)
     engine = postgres_hook.get_sqlalchemy_engine()
+
     row_count = deliveries_df.to_sql(pg_table, engine, schema=pg_schema, if_exists='append', index=False)
     print(f'{row_count} rows was inserted')
 
 with DAG(
         'get_api_data_to_staging',
         description='Выгрузка сырых данных из источника',
-        start_date=pendulum.datetime(2022, 5, 5, tz="UTC"), 
-        schedule_interval='0 * * * *',  # Расписание - загрузка 1 раз в час
-        catchup=False,
+        start_date=pendulum.datetime(2022, 12, 28, tz="UTC"), 
+        schedule_interval='0 0 * * *',  # Расписание - загрузка d 0.00 часов каждый день
+        catchup=True,
         tags=['sprint5_project', 'stg', 'api'], 
-        is_paused_upon_creation=True 
+        is_paused_upon_creation=True ,
+        max_active_runs=1 # Чтобы runs не мешались между собой и не конфликтовали в id
 ) as dag:
 
     reset_couriers = PostgresOperator(
         task_id='reset_couriers',
         postgres_conn_id=postgres_conn_id,
         sql="sql/reset_couriers.sql")
-
-    reset_deliveries = PostgresOperator(
-        task_id='reset_deliveries',
-        postgres_conn_id=postgres_conn_id,
-        sql="sql/reset_deliveries.sql")
 
     generate_couriers = PythonOperator(
         task_id='generate_couriers',
@@ -88,16 +113,19 @@ with DAG(
             'pg_schema':'stg'
             })
 
+    get_incr_ids = PythonOperator(
+        task_id='get_incr_ids',
+        python_callable=curr_delivery_id)
+
     generate_deliveries = PythonOperator(
         task_id='generate_deliveries',
         python_callable=get_deliveries,
-        do_xcom_push = False,
         op_kwargs={
-            'url': 'https://d5d04q7d963eapoepsqr.apigw.yandexcloud.net/deliveries?sort_field=order_id&sort_direction=asc&limit=50&offset=', 
             'pg_table':'api_deliveries', 
-            'pg_schema':'stg'
+            'pg_schema':'stg',
+            'date': business_dt
             })
 
 
 
-    reset_couriers >> generate_couriers >> reset_deliveries >> generate_deliveries 
+    reset_couriers >> generate_couriers >> get_incr_ids >> generate_deliveries 
--- src/dags/examples/project/stg_and_cdm_dag.py
+++ src/dags/examples/project/stg_and_cdm_dag.py
@@ -1,38 +1,135 @@
+import pandas as pd
+
 from airflow import DAG
 from airflow.providers.postgres.operators.postgres import PostgresOperator
+from airflow.providers.postgres.hooks.postgres import PostgresHook
+from airflow.operators.python_operator import PythonOperator
 import pendulum
+from sqlalchemy.sql.expression import bindparam
+from sqlalchemy.types import String
+from sqlalchemy.sql import text
+#import time
+#from datetime import datetime, timedelta
 
 postgres_conn_id = 'PG_WAREHOUSE_CONNECTION'
+business_dt = '{{ yesterday_ds }}' # за предыдущий день
+
+def deliveries_to_dds(business_dt):
+    postgres_hook = PostgresHook(postgres_conn_id)
+    engine = postgres_hook.get_sqlalchemy_engine()
+    sql = text(
+        """
+            insert into dds.dm_deliveries(order_id, delivery_id, courier_id, order_ts, delivery_ts, rate, tip_sum)
+            with raw as 
+	            (select replace(delivery_data, '''', '"') rawcol
+	            from stg.api_deliveries),
+            tab as 
+            	(select 
+            	rawcol::json->>'order_id' as order_id
+	            , rawcol::json->>'delivery_id' as delivery_id
+	            , rawcol::json->>'courier_id' as courier_id
+	            , (rawcol::json->>'order_ts')::timestamp as order_ts
+	            , (rawcol::json->>'delivery_ts')::timestamp as delivery_ts
+	            , (rawcol::json->>'rate')::float as rate
+	            , (rawcol::json->>'tip_sum')::float as tip_sum
+	            from raw)
+            select *
+            from tab
+            where cast(order_ts as date) = :business_dt;
+        """
+                ).bindparams(bindparam("business_dt", String))
+    engine.execute(sql, business_dt=business_dt)
+
+def load_main_report(business_dt):
+    postgres_hook = PostgresHook(postgres_conn_id)
+    engine = postgres_hook.get_sqlalchemy_engine()
+    sql = text(
+        """
+            DELETE FROM cdm.dm_courier_ledger 
+            WHERE settlement_year = extract('year' from cast(:business_dt as date))
+                and settlement_month = extract('month' from cast(:business_dt as date));
+            insert into cdm.dm_courier_ledger(courier_id, courier_name, settlement_year, settlement_month, orders_count, orders_total_sum, rate_avg, order_processing_fee, courier_order_sum, courier_tips_sum, courier_reward_sum)
+            with t as (
+            select 
+                c.courier_id
+                , c.courier_name
+                , d.order_id
+                , avg(1.0 * d.rate) over (partition by c.courier_id, extract('year' from d.order_ts), extract('month' from d.order_ts)) as rate_avg
+                , case when avg(1.0 * d.rate) over (partition by c.courier_id, extract('year' from d.order_ts), extract('month' from d.order_ts)) < 4.0 and o.total_sum * 0.05 >= 100 then o.total_sum * 0.05
+                    when avg(1.0 * d.rate) over (partition by c.courier_id, extract('year' from d.order_ts), extract('month' from d.order_ts)) < 4.0 and o.total_sum * 0.05 < 100 then 100
+                    when avg(1.0 * d.rate) over (partition by c.courier_id, extract('year' from d.order_ts), extract('month' from d.order_ts)) < 4.5 and o.total_sum * 0.07 >= 150 then o.total_sum * 0.07
+                    when avg(1.0 * d.rate) over (partition by c.courier_id, extract('year' from d.order_ts), extract('month' from d.order_ts)) < 4.5 and o.total_sum * 0.07 < 150 then o.total_sum * 0.07
+                    when avg(1.0 * d.rate) over (partition by c.courier_id, extract('year' from d.order_ts), extract('month' from d.order_ts)) < 4.9 and o.total_sum * 0.08 >= 175 then o.total_sum * 0.08
+                    when avg(1.0 * d.rate) over (partition by c.courier_id, extract('year' from d.order_ts), extract('month' from d.order_ts)) < 4.9 and o.total_sum * 0.08 < 175 then 175
+                    when avg(1.0 * d.rate) over (partition by c.courier_id, extract('year' from d.order_ts), extract('month' from d.order_ts)) >= 4.9 and o.total_sum * 0.1 < 200 then 200
+                    else o.total_sum * 0.1
+                    end as courier_order_sum
+                , d.tip_sum
+                , extract('year' from d.order_ts) as settlement_year
+                , extract('month' from d.order_ts) as settlement_month
+                , o.total_sum
+            from dds.dm_couriers c
+            left join dds.dm_deliveries d using (courier_id)
+            inner join 
+                (select order_key, sum(total_sum) as total_sum
+                from dds.fct_product_sales s
+                left join dds.dm_orders o on s.order_id = o.id
+                group by order_key) o on d.order_id = o.order_key 
+            ),
+            calc as (
+            select 
+                courier_id
+                , courier_name
+                , settlement_year
+                , settlement_month
+                , count(order_id) as orders_count
+                , sum(total_sum) as orders_total_sum
+                , rate_avg
+                , sum(total_sum) * 0.25 as order_processing_fee
+                , sum(courier_order_sum) as courier_order_sum
+                , sum(tip_sum) as courier_tips_sum
+                , sum(courier_order_sum) + sum(tip_sum) * 0.95 as courier_reward_sum
+            from t
+            group by courier_id, courier_name, settlement_year, settlement_month, rate_avg
+            )
+            select *
+            from calc
+            where settlement_year = extract('year' from cast(:business_dt as date))
+                and settlement_month = extract('month' from cast(:business_dt as date));;
+        """
+                ).bindparams(bindparam("business_dt", String))
+    engine.execute(sql, business_dt=business_dt)
 
 with DAG(
         'delivery_data_stg_to_dds_cdm',
         description='Обработка апишных json -> dds -> cdm',
-        start_date=pendulum.datetime(2022, 5, 5, tz="UTC"), 
-        schedule_interval='15 * * * *',  # Расписание - загрузка 1 раз в час - в 15 минут
-        catchup=False,
+        start_date=pendulum.datetime(2022, 12, 25, tz="UTC"), 
+        schedule_interval='0 1 * * *',  # Расписание - загрузка в 1 час ночи
+        catchup=True,
         tags=['sprint5_project', 'dds', 'cdm'], 
         is_paused_upon_creation=True 
 ) as dag:
 
-    reset_tables = PostgresOperator(
-        task_id='reset_tables',
+    load_couriers = PostgresOperator(
+        task_id='load_couriers',
         postgres_conn_id=postgres_conn_id,
-        sql="sql/stg_reset.sql")
+        sql="sql/dm_add_couriers.sql")
 
-    load_couriers_and_deliveries = PostgresOperator(
-        task_id='load_couriers_and_deliveries',
-        postgres_conn_id=postgres_conn_id,
-        sql="sql/dds.sql")
+    deliveries_to_dds = PythonOperator(
+            task_id='deliveries_to_dds',
+            python_callable=deliveries_to_dds,
+            do_xcom_push = False,
+            op_kwargs={
+                'business_dt':business_dt
+                })
 
-    reset_report = PostgresOperator(
-        task_id='reset_report',
-        postgres_conn_id=postgres_conn_id,
-        sql="sql/cdm_reset.sql")
-
-    load_report = PostgresOperator(
-        task_id='load_report',
-        postgres_conn_id=postgres_conn_id,
-        sql="sql/load_report_cdm.sql")
+    load_main_report = PythonOperator(
+            task_id='load_main_report',
+            python_callable=load_main_report,
+            do_xcom_push = False,
+            op_kwargs={
+                'business_dt':business_dt
+                })
 
 
-    reset_tables >> load_couriers_and_deliveries >> reset_report >> load_report
+    load_couriers >> deliveries_to_dds >> load_main_report
